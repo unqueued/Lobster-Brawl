b81dd25de00ab717555926e3c86b982f2f9105aa
Stella Liu <lius5225@gmail.com>
2017-03-21 18:23:04 -0400

    Initial Commit

---
+++ Assets/ButtonChecks.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ButtonChecks : MonoBehaviour {
+
+    public KeyCode keyT;
+    public KeyCode keyF;
+    public KeyCode keyG;
+    public KeyCode keyH;
+
+    public List<MoveButton> buttons = new List<MoveButton>();
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame  
+	void Update () {
+        //Checks through the list if button is in/on the bar line and if the correct button is pressed
+        for (int i = 0; i < buttons.Count; i++)
+        {
+            if (Input.GetKeyDown(keyT))
+            {
+                Debug.Log("Pressing T");
+                if (buttons[i].inRange == true && buttons[i].tag == "ButtonT")
+                {
+                    Destroy(buttons[i].gameObject);
+                }
+            }
+
+            if (Input.GetKeyDown(keyF))
+            {
+                Debug.Log("Pressing F");
+                if (buttons[i].inRange == true && buttons[i].tag == "ButtonF")
+                {
+                    Destroy(buttons[i].gameObject);
+                }
+            }
+
+            if (Input.GetKeyDown(keyG))
+            {
+                Debug.Log("Pressing G");
+                if (buttons[i].inRange == true && buttons[i].tag == "ButtonG")
+                {
+                    Destroy(buttons[i].gameObject);
+                }
+            }
+
+            if (Input.GetKeyDown(keyH))
+            {
+                Debug.Log("Pressing H");
+                if (buttons[i].inRange == true && buttons[i].tag == "ButtonH")
+                {
+                    Destroy(buttons[i].gameObject);
+                }
+            }
+        }//end of for loop
+    }
+}
---
+++ Assets/missBar.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class missBar : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    //Check if button enter the empty bar and gets destroyed
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "ButtonF" || other.gameObject.tag == "ButtonT" || other.gameObject.tag == "ButtonG" || other.gameObject.tag == "ButtonH")
+        {
+            Debug.Log("Empty Bar");
+            Destroy(other.gameObject);
+        }
+    }
+}
