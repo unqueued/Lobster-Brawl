72db8b7f7adcfdf6f3a8d9aad391970ea7ece2a0
ar3451 <ar3451@nyu.edu>
2017-04-06 13:10:06 -0400

    Add files via upload

---
+++ Assets/Scripts/Streak.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Streak : MonoBehaviour {
+
+    public static int hitStreak = 0;
+
+    public Transform comboDisplay;
+
+    public bool comboSpawn = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if ((hitStreak == 5) && (comboSpawn == false))
+        {
+            comboSpawn = true;
+            // Instantiate( combo picture goes here, new Vector3 (location goes here), 
+        }
+	}
+}
